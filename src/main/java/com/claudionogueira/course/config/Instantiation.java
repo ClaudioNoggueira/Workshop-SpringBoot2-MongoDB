cb59efa4ae3f620bc9eaadd3b171b2b6dce9d416
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-02-18 10:52:26 -0300

    Operação de instanciação de base de dados

---
+++ src/main/java/com/claudionogueira/course/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.claudionogueira.course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.claudionogueira.course.domain.User;
+import com.claudionogueira.course.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
