f349f8ccc49150a3b6473dc363a62d67b24f57a2
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-05-18 14:22:23 -0300

    Inserção de usuário com POST

--- src/main/java/com/claudionogueira/course/services/UserService.java
+++ src/main/java/com/claudionogueira/course/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.claudionogueira.course.domain.User;
+import com.claudionogueira.course.dto.UserDTO;
 import com.claudionogueira.course.repositories.UserRepository;
 import com.claudionogueira.course.services.exception.ObjectNotFoundException;
 
@@ -15,13 +16,21 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
