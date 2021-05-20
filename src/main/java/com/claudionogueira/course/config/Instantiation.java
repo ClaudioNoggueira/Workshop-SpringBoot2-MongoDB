0a4d5484f92a92fd4692c467b0cf9140bbd5b083
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-05-20 11:32:24 -0300

    Projeção dos dados com autor com DTO

--- src/main/java/com/claudionogueira/course/config/Instantiation.java
+++ src/main/java/com/claudionogueira/course/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.claudionogueira.course.domain.Post;
 import com.claudionogueira.course.domain.User;
+import com.claudionogueira.course.dto.AuthorDTO;
 import com.claudionogueira.course.repositories.PostRepository;
 import com.claudionogueira.course.repositories.UserRepository;
 
@@ -35,11 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
-				maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Acordei feliz hoje!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
