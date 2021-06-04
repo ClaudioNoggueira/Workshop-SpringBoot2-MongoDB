a574ed1fda4be93f7b965e41feff53d1e36e1156
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-06-04 16:21:41 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/claudionogueira/course/resources/UserResource.java
+++ src/main/java/com/claudionogueira/course/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.claudionogueira.course.domain.Post;
 import com.claudionogueira.course.domain.User;
 import com.claudionogueira.course.dto.UserDTO;
 import com.claudionogueira.course.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
