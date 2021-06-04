67ac860299a995b5a5ee7e265e51b48ee902071b
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-06-04 16:28:56 -0300

    Obtendo post por id

---
+++ src/main/java/com/claudionogueira/course/services/PostService.java
@@ -0,0 +1,22 @@
+package com.claudionogueira.course.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.claudionogueira.course.domain.Post;
+import com.claudionogueira.course.repositories.PostRepository;
+import com.claudionogueira.course.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
